596f3ad95f3a6852d5b7723d697354ec0ecfd28e
Fouad (Ayo) Animashaun <x16300351@student.ncirl.ie>
2019-12-17 11:08:14 +0000

    added project

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:un_pwn_able/screens/chat_screen.dart';
+import 'package:un_pwn_able/screens/landing_screen.dart';
+import 'package:un_pwn_able/screens/login_screen.dart';
+import 'package:un_pwn_able/screens/registration_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: LandingScreen.id,
+      routes: {
+        LandingScreen.id: (context) => LandingScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+      },
+    );
+  }
+}
+
